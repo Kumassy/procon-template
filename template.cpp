f73bccbf016ba3f2c59b85f45958c7fff058f5ae
Kumassy <kumassyii@gmail.com>
2017-05-30 00:06:26 +0900

    update template

--- template.cpp
+++ template.cpp
@@ -1,11 +1,31 @@
 #include <iostream>
+#include <cstdio>
+#include <cstdlib>
 #include <cassert>
+#include <string>
+#include <cstring>
+#include <vector>
+#include <list>
+#include <map>
+#include <set>
+#include <stack>
 #include <queue>
-
-#define INF 922337203685477580
-typedef long long ll;
+#include <algorithm>
 
 using namespace std;
+
+// ---------------------
+// repetition
+#define FOR(i,a,b) for(int i=(a);i<(b);++i)
+#define REP(i,n) FOR(i,0,n)
+
+// debug
+#define dump(x)  cerr << #x << " = " << (x) << endl;
+#define debug(x) cerr << #x << " = " << (x) << " (L" << __LINE__ << ")" << " " << __FILE__ << endl;
+
+// ---------------------
+#define INF 922337203685477580
+typedef long long ll;
 
 int main() {
   int X;
